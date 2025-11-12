72f679711f25bfde23498e023bca45b3365ad200
gkoteswar <gantakoti98@gmail.com>
2025-11-12 13:03:54 +0530

    intial commit

---
+++ Task_1_Perform_Basic_Mathematical_Operations.py
@@ -0,0 +1,12 @@
+num1=input("Enter the first number: ")
+num2=input("Enter the second number: ")
+
+num1=int(num1)
+num2=int(num2)
+
+print(f'''
+Addition: {num1+num2}
+Substraction: {num1-num2}
+Multiplication: {num1*num2}
+Division: {num1/num2}
+''')
